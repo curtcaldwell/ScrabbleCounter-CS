2ea97a64d759689ecd31e2d252cf189372b6ab10
Curt Caldwell and James Hanley <curt@>
2018-06-28 08:15:53 -0700

    Create Initial Project Folder.

---
+++ ScrabbleScore.Tests/ModelsTests/ScrabbleScoreTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleScore;
+
+namespace ScrabbleScore.Tests
+{
+  [TestClass]
+  public class ScrabbleScoreTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
+  
---
+++ ScrabbleScore/Models/ScrabbleScore.cs
@@ -0,0 +1,12 @@
+namespace ScrabbleScore
+{
+public class ScrabbleScore
+  {
+  public bool ExampleTest(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
+  
